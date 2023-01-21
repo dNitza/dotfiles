135e4662e454dd55a631fee5c1676442021a7d08
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-01-22 10:48:53 +1100

    Remove .config/fish/aliases.fish Remove .config/fish/completions/asdf.fish Remove .config/fish/completions/fisher.fish Remove .config/fish/conf.d/fish-ssh-agent.fish Remove .config/fish/conf.d/hydro.fish Remove .config/fish/conf.d/pisces.fish Remove .config/fish/conf.d/sponge.fish Remove .config/fish/conf.d/z.fish Remove .config/fish/config.fish Remove .config/fish/fish_plugins Remove .config/fish/fish_variables Remove .config/fish/functions/__bass.py Remove .config/fish/functions/__ssh_agent_is_started.fish Remove .config/fish/functions/__ssh_agent_start.fish Remove .config/fish/functions/__z.fish Remove .config/fish/functions/__z_add.fish Remove .config/fish/functions/__z_clean.fish Remove .config/fish/functions/__z_complete.fish Remove .config/fish/functions/_pisces_append.fish Remove .config/fish/functions/_pisces_backspace.fish Remove .config/fish/functions/_pisces_bind_pair.fish Remove .config/fish/functions/_pisces_complete.fish Remove .config/fish/functions/_pisces_insert_identical.fish Remove .config/fish/functions/_pisces_insert_left.fish Remove .config/fish/functions/_pisces_insert_right.fish Remove .config/fish/functions/_pisces_jump.fish Remove .config/fish/functions/_pisces_lookup.fish Remove .config/fish/functions/_pisces_remove.fish Remove .config/fish/functions/_pisces_should_insert.fish Remove .config/fish/functions/_pisces_skip.fish Remove .config/fish/functions/_sponge_clear_state.fish Remove .config/fish/functions/_sponge_on_exit.fish Remove .config/fish/functions/_sponge_on_postexec.fish Remove .config/fish/functions/_sponge_on_preexec.fish Remove .config/fish/functions/_sponge_on_prompt.fish Remove .config/fish/functions/_sponge_remove_from_history.fish Remove .config/fish/functions/bass.fish Remove .config/fish/functions/.DS_Store Remove .config/fish/functions/fish_mode_prompt.fish Remove .config/fish/functions/fish_prompt.fish Remove .config/fish/functions/fish_right_prompt.fish Remove .config/fish/functions/fish_title.fish Remove .config/fish/functions/fisher.fish Remove .config/fish/functions/sponge_filter_failed.fish Remove .config/fish/functions/sponge_filter_matched.fish Remove .config/fish/git_aliases.fish Remove .config/fish/setup.sh

--- private_dot_config/private_fish/functions/fish_prompt.fish
+++
@@ -1,4 +0,0 @@
-function fish_prompt -d Hydro
-    set_color green
-    string unescape "$_hydro_color_pwd$_hydro_pwd\x1b[0m $_hydro_color_git$$_hydro_git\x1b[0m$_hydro_color_duration$_hydro_cmd_duration\x1b[0m$_hydro_prompt\x1b[0m "
-end
