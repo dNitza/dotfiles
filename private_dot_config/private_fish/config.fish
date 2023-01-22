88d116bc07a848753fde3242744c092a76510dc5
Daniel Nitsikopoulos <daniel@dnitza.com>
2023-01-22 11:01:46 +1100

    Add .config/fish/aliases.fish Add .config/fish/completions/asdf.fish Add .config/fish/completions/fisher.fish Add .config/fish/conf.d/fish-ssh-agent.fish Add .config/fish/conf.d/hydro.fish Add .config/fish/conf.d/z.fish Add .config/fish/config.fish Update .config/fish/.DS_Store Add .config/fish/setup Add .config/fish/fish_plugins Add .config/fish/fish_variables Add .config/fish/functions/__ssh_agent_is_started.fish Add .config/fish/functions/__ssh_agent_start.fish Add .config/fish/functions/__z.fish Add .config/fish/functions/__z_add.fish Add .config/fish/functions/__z_clean.fish Add .config/fish/functions/__z_complete.fish Add .config/fish/functions/fish_mode_prompt.fish Add .config/fish/functions/fish_prompt.fish Add .config/fish/functions/fisher.fish Add .config/fish/git_aliases.fish Add .config/fish/themes/.keep

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,18 @@
+source ~/.config/fish/aliases.fish
+
+function fish_user_key_bindings
+  # Thanks, https://github.com/fish-shell/fish-shell/issues/905#issuecomment-20559486
+  bind \cc 'echo; commandline ""; commandline -f repaint'
+  bind \cg 'test -d .git; and git checkout (string trim -- (git branch | fzf)); and commandline -f repaint'
+  bind \cG 'test -d .git; and git checkout (string trim -- (git branch --all | fzf)); and commandline -f repaint'
+end
+
+set -Ux EDITOR vim
+
+source ~/.asdf/asdf.fish
+
+set -Ux FZF_DEFAULT_COMMAND 'rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,.git/*}"';
+set -gx GPG_TTY (tty)
+
+set -gx PATH "$HOME/.cargo/bin" $PATH;
+set -gx PATH "$HOME/.yarn/bin" $PATH;
